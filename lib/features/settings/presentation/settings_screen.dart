06b74dc6a634d32b1cca7b3193fa610aff4f4ea8
GASTDASH <gastdash@gmail.com>
2025-06-22 14:27:46 +0300

    Экран настроек

--- lib/features/settings/presentation/settings_screen.dart
+++ lib/features/settings/presentation/settings_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:pricecalc/core/core.dart';
+import 'package:pricecalc/features/settings/presentation/widgets/setting_card.dart';
+import 'package:talker_flutter/talker_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -11,7 +16,54 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(slivers: [SliverAppBar(title: Text("Настройки"))]),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(title: Text("Настройки")),
+          SliverPadding(
+            padding: const EdgeInsets.all(12),
+            sliver: SliverList.list(
+              children: [
+                SettingCard(
+                  icon: Icons.bug_report_outlined,
+                  title: "Сообщить об ошибке",
+                  onTap: () async {
+                    await showBugReportDialog(context);
+                  },
+                ),
+                SizedBox(height: 12),
+                SettingCard(
+                  icon: Icons.info_outline,
+                  title: "О приложении",
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<dynamic> showBugReportDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder:
+          (context) => DialogCustom(
+            title: "Отчёт об ошибке",
+            text:
+                "Приложение всё ещё в разработке, поэтому мы будем рады, если вы напишите об найденной ошибке. Мы попытаемся её исправить в кратчайшие сроки.",
+            child: ButtonCustom(
+              text: "Написать",
+              onTap: () async {
+                final Uri uri = Uri.parse(
+                  "https://github.com/GASTDASH/pricecalc/issues",
+                );
+                if (!await launchUrl(uri)) {
+                  GetIt.I<Talker>().error("Не удалось открыть ссылку $uri");
+                }
+              },
+            ),
+          ),
     );
   }
 }
---
+++ lib/features/settings/presentation/widgets/setting_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class SettingCard extends StatelessWidget {
+  const SettingCard({
+    super.key,
+    this.onTap,
+    required this.title,
+    required this.icon,
+  });
+
+  final void Function()? onTap;
+  final String title;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Material(
+      elevation: 2,
+      borderRadius: BorderRadius.circular(12),
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(12),
+        child: Ink(
+          padding: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            // border: Border.all(color: theme.hintColor),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                spacing: 14,
+                children: [
+                  Icon(icon, size: 42),
+                  Text(title, style: theme.textTheme.titleLarge),
+                ],
+              ),
+              Icon(Icons.chevron_right_rounded),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
